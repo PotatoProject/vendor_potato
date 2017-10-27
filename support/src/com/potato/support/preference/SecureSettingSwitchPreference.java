4ca8fb4d3de2f23a5d9e82e1049bad4040d875ac
Alex Cruz <alex@dirtyunicorns.com>
2017-10-26 23:03:07 -0400

    Update imports for preferences
    Change-Id: I3435a9214b12615f5198ac304943f1860d7eeb77
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- support/src/com/potato/support/preference/SecureSettingSwitchPreference.java
+++ support/src/com/potato/support/preference/SecureSettingSwitchPreference.java
@@ -17,7 +17,7 @@
 package com.potato.support.preference;
 
 import android.content.Context;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
