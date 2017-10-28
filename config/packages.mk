87bff9a6a9719ec2287bd87d210f73b92b38f583
bigrushdog <randall.rushing@gmail.com>
2017-10-28 16:43:00 -0700

    DUI: Initial checkin for Oreo [2/7]
    Change-Id: Ieca38bd1cf92e4d75d65a22657b4c8d30cd41f6e

--- config/packages.mk
+++ config/packages.mk
@@ -4,3 +4,11 @@ PRODUCT_PACKAGES += \
 # Include librsjni explicitly to workaround GMS issue
 PRODUCT_PACKAGES += \
     librsjni
+
+# DU Utils Library
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
+
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
