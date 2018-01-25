3f58c9ce4c81ebdb728edab76bddf272d02215c9
Alex Cruz <du.alexcruz@gmail.com>
2018-01-25 06:24:57 -0500

    Add Gboard overlay [1/2]
    Change-Id: Ibaf2e7455ce33d52b429d8ea44f5773cd82a2d16
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -24,4 +24,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SettingsDarkThemeOverlay \
     SystemDarkThemeOverlay \
-    SysuiQsDarkThemeOverlay
+    SysuiQsDarkThemeOverlay \
+    GBoardDarkTheme
