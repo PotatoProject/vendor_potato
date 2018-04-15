DirtyUnicorns/android_vendor_du

Fix Google dialer
Add in dialer_experience.xml found in the factory image so if the user
decides to install it, they're not meant with a message about how is not
supported.

Change-Id: I372ff394e3704dd6380812140a4e35cfbfb8f852

config/common.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc

PotatoProject/vendor_potato

config/common.mk
@@ -36,5 +36,9 @@ PRODUCT_COPY_FILES += \
 endif
 endif
 
+# Fix Google dialer
+PRODUCT_COPY_FILES += \
+    vendor/potato/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Packages
 include vendor/potato/config/packages.mk