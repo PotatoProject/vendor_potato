b14dc300cf58b3cf67587fc295d73e25482ad8e2
YOUNG HO CHA <ganadist@gmail.com>
2018-06-26 00:46:49 -0500

    vendor: Lock rotation on SetupWizard
    This avoids any jank that may come from setup wizards that are not
    optimized for landscape views.
    
    Change-Id: I5f5506ced71deda3ba0e44a222c7d6b21907460f

--- config/common.mk
+++ config/common.mk
@@ -26,7 +26,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.virtual=0 \
     ro.build.selinux=1 \
     ro.boot.vendor.overlay.theme=com.potato.overlay.accent.purple;com.potato.overlay.base.stockfixed;com.potato.overlay.lawnconf;com.potato.overlay.shape.circle \
-    ro.opa.eligible_device=true
+    ro.opa.eligible_device=true \
+    ro.setupwizard.rotation_locked=true
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
