158eb6bfab9c35e0f9facc1000db9b426c792a8a
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-13 16:50:35 +0000

    potato: WIP
    Change-Id: Ic721335dfe7a8db5d72e3ee2a105ee2ffc90e3e6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -5,43 +5,36 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librsjni
 
-# DU Utils Library
-PRODUCT_PACKAGES += \
-    org.dirtyunicorns.utils
-
-PRODUCT_BOOT_JARS += \
-    org.dirtyunicorns.utils
-
-# Accent and Base packages
-PRODUCT_PACKAGES += \
-    AmberTheme \
-    BlueTheme \
-    CyanTheme \
-    DeepOrangeTheme \
-    DeepPurpleTheme \
-    GreenTheme \
-    IndigoTheme \
-    LightBlueTheme \
-    LightGreenTheme \
-    LimeTheme \
-    OrangeTheme \
-    PinkTheme \
-    PixelBaseTheme \
-    PixelTheme \
-    PurpleTheme \
-    RedTheme \
-    StockBaseTheme \
-    StockFixedBaseTheme \
-    StockTheme \
-    YellowTheme
-
-# Dark/BlackUI Packages
-PRODUCT_PACKAGES += \
-    GBoardDarkTheme \
-    SettingsBlackTheme \
-    SettingsDarkTheme \
-    SystemBlackTheme \
-    SystemDarkTheme \
-    SystemSettingsIconTint \
-	SysuiQsBlackTheme \
-    SysuiQsDarkTheme
+## Accent and Base packages
+#PRODUCT_PACKAGES += \
+#    AmberTheme \
+#    BlueTheme \
+#    CyanTheme \
+#    DeepOrangeTheme \
+#    DeepPurpleTheme \
+#    GreenTheme \
+#    IndigoTheme \
+#    LightBlueTheme \
+#    LightGreenTheme \
+#    LimeTheme \
+#    OrangeTheme \
+#    PinkTheme \
+#    PixelBaseTheme \
+#    PixelTheme \
+#    PurpleTheme \
+#    RedTheme \
+#    StockBaseTheme \
+#    StockFixedBaseTheme \
+#    StockTheme \
+#    YellowTheme
+#
+## Dark/BlackUI Packages
+#PRODUCT_PACKAGES += \
+#    GBoardDarkTheme \
+#    SettingsBlackTheme \
+#    SettingsDarkTheme \
+#    SystemBlackTheme \
+#    SystemDarkTheme \
+#    SystemSettingsIconTint \
+#	SysuiQsBlackTheme \
+#    SysuiQsDarkTheme
