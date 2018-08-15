77d5dfb209ad2ffb50cb98d79ef6923038359b48
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-15 19:16:40 +0000

    potato: Don't add full-eng
    - Keep things clean :)
    
    Change-Id: I76ebee55e14db11e9259c5cd1245645dfe7a8fa5
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -68,7 +68,6 @@ function breakfast()
     local variant=$2
     POTATO_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
-    add_lunch_combo full-eng
     for f in `/bin/ls vendor/potato/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
