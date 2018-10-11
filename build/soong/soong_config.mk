d80df736f234b8d13fea1a3dff8504bfaca84551
Alessandro Astone <ales.astone@gmail.com>
2018-10-12 00:16:27 +0200

    soong_config: Add BOOTLOADER_MESSAGE_OFFSET
    * libbootloader_message now uses Android.bp
    
    Change-Id: I750a04be97dbe01564d45cd605744b15f209e408

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,9 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
 _contents := $(_contents)    "Potato":{$(newline)
 
+$(call add_json_val_default, Bootloader_message_offset, $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
