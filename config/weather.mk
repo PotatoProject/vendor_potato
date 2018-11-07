5d94893660d5ed2c0bac8cf44757d7c00ff87213
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-11-07 20:27:12 -0200

    Add default permissions
    Change-Id: Icba08d4b98452b9d57c771e1719fb002c7019cf5

--- config/weather.mk
+++ config/weather.mk
@@ -1,3 +1,3 @@
 PRODUCT_COPY_FILES += \
-    vendor/potato/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml
-
+    vendor/potato/etc/permissions/org.pixelexperience.weather.client.xml:system/etc/permissions/org.pixelexperience.weather.client.xml \
+    vendor/potato/etc/default-permissions/org.pixelexperience.weather.client.xml:system/etc/default-permissions/org.pixelexperience.weather.client.xml
