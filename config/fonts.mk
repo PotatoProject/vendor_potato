7016c9ffc817ebdc792d50d89d8e5e47abdb5abd
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-02-17 21:18:16 +0530

    Add Google Sans font
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>
    Change-Id: Icbd0f342d8f7e5366df37fabf0cbf8bf3a9986c2

---
+++ config/fonts.mk
@@ -0,0 +1,10 @@
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/potato/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/potato/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/potato/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/potato/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/potato/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/potato/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+ADDITIONAL_FONTS_FILE := vendor/potato/fonts/google-sans.xml
