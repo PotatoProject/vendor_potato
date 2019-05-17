83691b4a7cfd9b1503b0ddaa3978ae93aad133f2
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-17 12:03:57 -0700

    potato: qcom: Add UM 4.14 platforms
    * Note: msmnile is treated like msmnile because unlike other Qualcomm
      codenames msmnile does not expand to 8150.
    
    Change-Id: If246c0ec06de22097d369c17ce48aa1270bab19c
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- config/ProductConfigQcom.mk
+++ config/ProductConfigQcom.mk
@@ -1,13 +1,19 @@
 # Bring in Qualcomm helper macros
 include vendor/potato/build/core/qcom_utils.mk
 
+# Platform names
+MSMNILE := msmnile #SM8150
+MSMSTEPPE := sm6150
+TRINKET := trinket #SM6125
+
 B_FAMILY := msm8226 msm8610 msm8974
 B64_FAMILY := msm8992 msm8994
 BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY)
+UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET)
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
 BOARD_USES_ADRENO := true
 
@@ -39,12 +45,12 @@ TARGET_USES_COLOR_METADATA := true
 endif
 
 # Enable DRM PP driver on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
 TARGET_USES_DRM_PP := true
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 $(UM_4_14_FAMILY)
 
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
 MSM_VIDC_TARGET_LIST := $(B_FAMILY)
@@ -70,8 +76,13 @@ ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
 MSM_VIDC_TARGET_LIST := $(UM_4_9_FAMILY)
 QCOM_HARDWARE_VARIANT := sdm845
 else
+ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+MSM_VIDC_TARGET_LIST := $(UM_4_14_FAMILY)
+QCOM_HARDWARE_VARIANT := sm8150
+else
 MSM_VIDC_TARGET_LIST := $(PRODUCT_BOARD_PLATFORM)
 QCOM_HARDWARE_VARIANT := $(PRODUCT_BOARD_PLATFORM)
+endif
 endif
 endif
 endif
