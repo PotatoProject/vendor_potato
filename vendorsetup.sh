d4d3f1ea60965a0cc9a1f4f61ab9270ae8c62edc
Kshitij Gupta <kshitijgm@gmail.com>
2019-06-01 14:09:24 +0000

    potato: Cook user builds by default
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: Ib3b46eef0f10ca61431d817d1bdbb59e61bff559

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-for device in $(cat $(gettop)/vendor/potato/potato.devices | sed -e 's/#.*$//' | awk '{printf "potato_%s-userdebug\n", $1}'); do
+for device in $(cat $(gettop)/vendor/potato/potato.devices | sed -e 's/#.*$//' | awk '{printf "potato_%s-user\n", $1}'); do
     add_lunch_combo $device;
 done;
