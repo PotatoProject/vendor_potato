ff6f1171d1536817294454ac60740ffcf9ceda31
Kshitij Gupta <kshitijgm@gmail.com>
2019-06-01 17:00:59 +0000

    potato: config: Move out branding to it's own file
    - Also make use of PRODUCT_SYSTEM_DEFAULT_PROPERTIES instead of
      PRODUCT_PROPERTY_OVERRIDES to ensure the prop is added to the
      system prop-file
    
    Change-Id: I9e29b251f119f5ef95ba849eea4ca7d3f99653c0
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -42,57 +42,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.potato.overlay.accent.purple;com.potato.overlay.base.stockfixed;com.potato.overlay.lawnconf \
     ro.opa.eligible_device=true
 
-ifndef BUILD_TYPE
-    BUILD_TYPE := COMMUNITY
-endif
-
-ifndef BUILD_STATE
-    BUILD_STATE := UNKNOWN
-endif
-
-POTATO_DISH := Baked
-POTATO_VERNUM := 2.3
-ifeq ($(USE_TIME_IN_NAME), true)
-    ifeq ($(BUILD_TYPE), COMMUNITY)
-       POTATO_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d_%H%M)
-    endif
-endif
-
-ifndef POTATO_VERSION
-    POTATO_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)
-endif
-
-CURRENT_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
-LIST := $(shell cat vendor/potato/potato.devices)
-
-ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-    ifeq ($(filter-out OFFICIAL WEEKLY MASHED, $(BUILD_TYPE)),)
-        POTATO_VERSION := $(POTATO_VERSION).$(POTATO_DISH)-v$(POTATO_VERNUM)
-        ifeq ($(BUILD_TYPE), MASHED)
-          BUILD_STATE := TEST
-        endif
-        ifeq ($(filter-out EXPERIMENTAL EXPERIMENTS TESTING TEST, $(BUILD_STATE)),)
-            POTATO_VERSION :=$(POTATO_VERSION).MASHED
-            PRODUCT_PROPERTY_OVERRIDES += ro.potato.type=mashed
-        else
-            ifeq ($(BUILD_TYPE), WEEKLY)
-              POTATO_VERSION :=$(POTATO_VERSION).WEEKLY
-              PRODUCT_PROPERTY_OVERRIDES += ro.potato.type=weekly
-            endif
-        endif
-    else
-        POTATO_VERSION := $(POTATO_VERSION).CHIPS-v$(POTATO_VERNUM).$(BUILD_TYPE)
-    endif
-else
-    ifeq ($(filter-out OFFICIAL WEEKLY, $(BUILD_TYPE)),)
-      $(error "Invalid BUILD_TYPE!")
-    endif
-    POTATO_VERSION := $(POTATO_VERSION).CHIPS-v$(POTATO_VERNUM).$(BUILD_TYPE)
-endif
-
-export POTATO_VERNUM
-export POTATO_DISH
-
 # LatinIME gesture typing
 ifneq ($(filter tenderloin,$(TARGET_PRODUCT)),)
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
@@ -188,3 +137,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/potato/overlay/common
 
 # Packages
 include vendor/potato/config/packages.mk
+
+# Branding
+include vendor/potato/config/branding.mk
