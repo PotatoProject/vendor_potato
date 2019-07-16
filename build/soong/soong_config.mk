5959123bfd783ded1e1608f0cf3318f8c9b9685f
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-16 15:58:42 -0300

    vendor: Add qcom platform type to soongs vars
    Required for new commits on frameworks/native
    
    * Fix lag during screen recording on some platforms
    * Fix screen flickering after disconnecting from Android Auto on some platforms
    
    Change-Id: Ia0ed0bbfda44fb5ccb184ace854ec1bf9a7d819a

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -18,6 +18,11 @@ $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PRO
 $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Uses_qcom_um_family, $(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_3_18_family, $(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_4_family, $(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_9_family, $(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
+$(call add_json_bool, Uses_qcom_um_4_14_family, $(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
