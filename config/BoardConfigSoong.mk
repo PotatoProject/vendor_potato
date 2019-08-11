f6f044bf31a93fcec10272d5145f732aec1c5fff
Kshitij Gupta <kshitijgm@gmail.com>
2019-08-11 17:31:07 +0530

    header-gen: Use MAKE_PREBUILT and expose origpath to soong
    - Much like kernel compilation, we need the system path for invoking
      gcc. Load origpath for kernel header generation, and expose that along
      with MAKE_PREBUILT to soong.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I08b88623edb5ca7f113cb7eac35137a821818a4b

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,4 +1,5 @@
 # Add variables that we wish to make available to soong here.
+ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
 EXPORT_TO_SOONG := \
     KERNEL_ARCH \
     KERNEL_CC \
@@ -6,7 +7,9 @@ EXPORT_TO_SOONG := \
     KERNEL_CROSS_COMPILE \
     KERNEL_MAKE_FLAGS \
     TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE
+    TARGET_KERNEL_SOURCE \
+    MAKE_PREBUILT \
+    ORIG_PATH
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
