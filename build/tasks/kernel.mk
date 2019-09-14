9c98ef7405b16147b91e5b90f1f647db2e498593
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-14 16:26:26 +0200

    kernel: Fix specifying custom clang compiler version
    * Folder layout name changed in Q
    
    Change-Id: I823867334e87fbb3c240a92e62484b6531dfcd22

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -166,8 +166,7 @@ PATH_OVERRIDE := PATH=$(shell cat $(OUT_DIR)/.path_interposer_origpath):$$PATH
 
 ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     ifneq ($(TARGET_KERNEL_CLANG_VERSION),)
-        # Find the clang-* directory containing the specified version
-        KERNEL_CLANG_VERSION := $(shell find $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/ -name AndroidVersion.txt -exec grep -l $(TARGET_KERNEL_CLANG_VERSION) "{}" \; | sed -e 's|/AndroidVersion.txt$$||g;s|^.*/||g')
+        KERNEL_CLANG_VERSION := clang-$(TARGET_KERNEL_CLANG_VERSION)
     else
         # Use the default version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
         KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
