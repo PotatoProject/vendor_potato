5db8a6fa360b2f82db304749f7fad33a557614dc
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 20:57:26 +0300

    build: Remove ability to set WLAN/BT_VENDOR variant
    * These flags have no users since cm-14.1
    
    Change-Id: Ia2b2fd5d95e856fdae3ec23532bc6eceff2910c6

--- build/core/pathmap.mk
+++ build/core/pathmap.mk
@@ -8,19 +8,6 @@ $(eval pathmap_PROJ += $(1):$(2)) \
 $(eval PATHMAP_SOONG_NAMESPACES += $(2))
 endef
 
-# Enter variant project path into pathmap
-#
-# $(1): name
-# $(2): variable to check
-# $(3): base path
-#
-define project-set-path-variant
-    $(call project-set-path,$(1),$(strip \
-        $(if $($(2)), \
-            $(3)-$($(2)), \
-            $(3))))
-endef
-
 # Returns the path to the requested module's include directory,
 # relative to the root of the source tree.
 #
