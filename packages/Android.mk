20c4e50ee18a0628eaf33c115670280e15566ddf
Kshitij Gupta <kshitijgm@gmail.com>
2019-10-05 01:30:48 +0530

    [DNM] packages: Add target for framework jar
    - Useful when working on system apps in studio :)
    
    Change-Id: I19b8dd89554670127d2ffd776dad6065c81aee4d
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/Android.mk
+++ packages/Android.mk
@@ -1,3 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := FrameworkLibs
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+framework_deps := $(call java-lib-deps,framework)
+framework_libs_path := $(PRODUCT_OUT)/framework-libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+.PHONY: copy_framework_deps
+copy_framework_deps: $(framework_deps)
+	$(hide) mkdir -p $(framework_libs_path)
+	$(hide) rm -rf $(framework_libs_path)/*.jar
+	$(hide) cp $(framework_deps) $(framework_libs_path)/framework.jar
+
+$(LOCAL_BUILT_MODULE): copy_framework_deps
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
