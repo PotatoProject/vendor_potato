95e75a0c8b88016a0f0d5a80f6d52ff57e3fbfc8
ezio84 <brabus84@gmail.com>
2019-11-17 20:21:22 +0100

    Remove BUILD_UTC_DATE=0 flag
    Change-Id: I7a65d6d0c7eda086c6d1d1281538f80705c87c1f

--- config/common.mk
+++ config/common.mk
@@ -2,8 +2,6 @@ include vendor/potato/config/ProductConfigQcom.mk
 
 PRODUCT_SOONG_NAMESPACES += $(PATHMAP_SOONG_NAMESPACES)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google
