18f71f24c1309f858b81fd51ccb7ee4e4a84ea86
rajadeja <raj.jdja@gmail.com>
2019-11-26 08:35:47 +0000

    branding: simplify it
    Change-Id: I02ffb8cd1bce1f133bbe60c441f695561dbe7227

--- config/branding.mk
+++ config/branding.mk
@@ -1,58 +1,44 @@
-# PotatoCenter
-ifeq ($(filter-out OFFICIAL WEEKLY MASHED, $(BUILD_TYPE)),)
-    PRODUCT_PACKAGES += \
-        PotatoCenter
+POTATO_VERNUM = 3.0.0-beta+3
+POTATO_DISH = croquette
+
+ifndef BUILD_TYPE
+    BUILD_TYPE := COMMUNITY
 endif
 
 ifneq ($(SIGNING_KEYS),)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := $(SIGNING_KEYS)/releasekey
 endif
 
-ifndef BUILD_TYPE
-    BUILD_TYPE := COMMUNITY
-endif
-
-ifndef BUILD_STATE
-    BUILD_STATE := UNKNOWN
-endif
-
-POTATO_DISH := croquette
-POTATO_VERNUM := 3.0.0-beta+3
-ifeq ($(USE_TIME_IN_NAME), true)
-    ifeq ($(BUILD_TYPE), COMMUNITY)
-       POTATO_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d_%H%M)
-    endif
-endif
-
-ifndef POTATO_VERSION
-    POTATO_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)
-endif
-
 CURRENT_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 LIST := $(shell cat vendor/potato/potato.devices)
 
 ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-    ifeq ($(filter-out OFFICIAL WEEKLY MASHED, $(BUILD_TYPE)),)
-        POTATO_VERSION := $(POTATO_VERSION).$(POTATO_DISH)-v$(POTATO_VERNUM)
+    ifeq ($(filter-out OFFICIAL MASHED, $(BUILD_TYPE)),)
+        ifeq ($(BUILD_TYPE), OFFICIAL)
+          BUILD_TYPE := OFFICIAL
+        endif
         ifeq ($(BUILD_TYPE), MASHED)
-          BUILD_STATE := TEST
+          BUILD_TYPE := MASHED
         endif
-        ifeq ($(filter-out EXPERIMENTAL EXPERIMENTS TESTING TEST, $(BUILD_STATE)),)
-            POTATO_VERSION :=$(POTATO_VERSION).MASHED
-            PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.potato.type=mashed
-        else
-            PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.potato.type=$(POTATO_DISH)
-        endif
-    else
-        POTATO_VERSION := $(POTATO_VERSION).CHIPS-v$(POTATO_VERNUM).$(BUILD_TYPE)
     endif
 else
-    ifeq ($(filter-out OFFICIAL WEEKLY, $(BUILD_TYPE)),)
+    ifeq ($(filter-out OFFICIAL MASHED, $(BUILD_TYPE)),)
       $(error "Invalid BUILD_TYPE!")
     endif
-    POTATO_VERSION := $(POTATO_VERSION).CHIPS-v$(POTATO_VERNUM).$(BUILD_TYPE)
 endif
 
-export POTATO_VERNUM
-export POTATO_DISH
+ifeq ($(filter-out OFFICIAL MASHED, $(BUILD_TYPE)),)
+    PRODUCT_PACKAGES += \
+        PotatoCenter
+endif
 
+POTATO_VERSION := $(TARGET_PRODUCT)-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(POTATO_DISH).v$(POTATO_VERNUM).$(BUILD_TYPE)
+
+POTATO_BRANDING_VERSION := $(BUILD_TYPE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+  ro.potato.version=$(POTATO_VERSION) \
+  ro.potato.buildtype=$(BUILD_TYPE) \
+  ro.potato.vernum=$(POTATO_VERNUM) \
+  ro.potato.dish=$(POTATO_DISH) \
+  ro.potato.branding.version=$(POTATO_BRANDING_VERSION)
