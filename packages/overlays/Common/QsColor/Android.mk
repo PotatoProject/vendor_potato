22749c5c4e591b3b4b17efe82b2f40bd694be1be
Kshitij Gupta <kshitijgm@gmail.com>
2019-12-01 18:50:43 +0000

    overlays: Refactor and redo QS Color overlay
    Change-Id: Ib49ef93a847b32de1facfb879c0dc91c5520d274
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/overlays/Common/QsColor/Android.mk
+++ packages/overlays/Common/QsColor/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := QsAccentBlack
+LOCAL_PACKAGE_NAME := QsColor
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
