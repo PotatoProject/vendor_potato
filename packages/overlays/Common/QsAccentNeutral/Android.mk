3845f6cac11291fe5647fcaa8c534426f988b148
Kshitij Gupta <kshitijgm@gmail.com>
2019-12-01 18:50:43 +0000

    overlays: Refactor and redo QS Accent
    Change-Id: Ib49ef93a847b32de1facfb879c0dc91c5520d274
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/overlays/Common/QsAccentNeutral/Android.mk
+++ packages/overlays/Common/QsAccentNeutral/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := QsAccentBlack
+LOCAL_PACKAGE_NAME := QsAccentNeutral
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
