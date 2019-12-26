78988ff7846c19f939451f037820b5b54e4d6ed4
Kshitij Gupta <kshitijgm@gmail.com>
2019-12-26 12:32:43 +0530

    potato: Whitelist hiddenapi for fries
    Change-Id: I25e1131e5c6cb46e61715c5569d81122e0fcd8a8

--- config/common.mk
+++ config/common.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
     vendor/potato/prebuilt/common/etc/permissions/privapp-permissions-potato.xml:system/etc/permissions/privapp-permissions-potato.xml \
     vendor/potato/prebuilt/common/etc/permissions/co.potatoproject.posp.xml:system/etc/permissions/co.potatoproject.posp.xml
 
+# Fries
+PRODUCT_COPY_FILES += \
+    vendor/potato/prebuilt/common/etc/sysconfig/potatofries-hiddenapi-package-whitelist.xml:system/etc/sysconfig/potatofries-hiddenapi-package-whitelist.xml
+
 # Fix Google dialer
 PRODUCT_COPY_FILES += \
     vendor/potato/prebuilt/common/etc/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
