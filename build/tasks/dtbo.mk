d5f3388b81c031cff225a2479718e418873eb587
Kshitij Gupta <kshitijgm@gmail.com>
2020-01-02 07:11:28 +0000

    kernel: Add support for dtbo generation from cfg
    Change-Id: I31cf7cef704d5c8d5409f9f265b127feeeb16890

--- build/tasks/dtbo.mk
+++ build/tasks/dtbo.mk
@@ -18,8 +18,14 @@ define build-dtboimage-target
     $(hide) chmod a+r $@
 endef
 
+define build-dtboimage-target-from-cfg
+    $(call pretty,"Target dtbo image from cfg: $(BOARD_PREBUILT_DTBOIMAGE)")
+    $(hide) $(MKDTIMG) cfg_create $@ $(KERNEL_OUT)/$(BOARD_KERNEL_DTBO_CFG)
+    $(hide) chmod a+r $@
+endef
+
 $(BOARD_PREBUILT_DTBOIMAGE): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
-	$(build-dtboimage-target)
+	$(if $(BOARD_KERNEL_DTBO_CFG), $(build-dtboimage-target-from-cfg), $(build-dtboimage-target))
 
 endif # BOARD_KERNEL_SEPARATED_DTBO
 endif # TARGET_NO_KERNEL
