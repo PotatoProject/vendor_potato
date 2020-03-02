b854ffefb5881f80e4759fafea7824dec50edb0a
ezio84 <brabus84@gmail.com>
2020-03-02 17:06:47 +0000

    Use a custom build date-time prop to refresh packages cache [2/2]
    [@AgentFabulous | POSP]
    - Fix pickup of build datetime
    
    Change-Id: Ia44ac78e821763c71db60273134849b76dfb6686
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- build/tools/buildinfo.sh
+++ build/tools/buildinfo.sh
@@ -11,5 +11,6 @@ echo "ro.potato.buildtype=$($GET_VERSION buildtype)"
 echo "ro.potato.dish=$($GET_VERSION dish)"
 echo "ro.potato.vernum=$($GET_VERSION vernum)"
 echo "ro.potato.version=$($GET_VERSION version)"
+echo "ro.build.datetime=`$DATE +%s`"
 
 echo "# end potato build properties"
