7743ebba9e61762c97a048bc1f7d1c5a2bde9830
Lucas Dupin <dupin@google.com>
2020-03-14 14:44:40 -0700

    potato: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866

--- config/common.mk
+++ config/common.mk
@@ -158,3 +158,9 @@ include vendor/potato/config/packages.mk
 
 # Branding
 include vendor/potato/config/branding.mk
+
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
