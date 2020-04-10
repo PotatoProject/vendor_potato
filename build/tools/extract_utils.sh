d7e2574a6bd3867b653bfcc2f43ce16b5c5b2861
TheStrix <parthbhatia98@gmail.com>
2020-04-10 18:20:19 +0530

    extract_utils: Add support for overriding packages
     * An example of how this can be used is say a device
       wants to use the original OEM camera, e.g. Essential PH-1.
       Klik is required in order to use the external camera
       module that you could buy for the device so it's preferred
       to use it over Camera2 or Snap.
    
      * An example of how to use this argument:
        -system/priv-app/Klik/Klik.apk;OVERRIDES=Camera2
    
    also includes
    
    From 72d5bdfb73ca80089af4f243c252ecde210d357f Mon Sep 17 00:00:00 2001
    From: LuK1337 <priv.luk@gmail.com>
    Date: Mon, 23 Aug 2021 18:18:57 +0200
    Subject: [PATCH] fixup! extract_utils: Add support for overriding packages
    
    This unifies arguments parsing + fixes case where OVERRIDES=pkg would
    generate .bp without `certificate: "platform"`.
    
    Change-Id: I2504cbb760b20ed86c2dc088cc4bd74b78ba64d9

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -357,6 +357,7 @@ function write_blueprint_packages() {
     local EXTENSION=
     local PKGNAME=
     local SRC=
+    local OVERRIDEPKG=
 
     for P in "${FILELIST[@]}"; do
         FILE=$(target_file "$P")
@@ -422,11 +423,22 @@ function write_blueprint_packages() {
                 SRC="$SRC/app"
             fi
             printf '\tapk: "%s/%s",\n' "$SRC" "$FILE"
-            if [ "$ARGS" = "PRESIGNED" ]; then
-                printf '\tpresigned: true,\n'
-            elif [ ! -z "$ARGS" ]; then
-                printf '\tcertificate: "%s",\n' "$ARGS"
-            else
+            ARGS=(${ARGS//;/ })
+            USE_PLATFORM_CERTIFICATE="true"
+            for ARG in "${ARGS[@]}"; do
+                if [ "$ARG" = "PRESIGNED" ]; then
+                    USE_PLATFORM_CERTIFICATE="false"
+                    printf '\tpresigned: true,\n'
+                elif [[ "$ARG" =~ "OVERRIDES" ]]; then
+                    OVERRIDEPKG=${ARG#*=}
+                    OVERRIDEPKG=${OVERRIDEPKG//,/ }
+                    printf '\toverrides: ["%s"],\n' "$OVERRIDEPKG"
+                elif [ ! -z "$ARG" ]; then
+                    USE_PLATFORM_CERTIFICATE="false"
+                    printf '\tcertificate: "%s",\n' "$ARG"
+                fi
+            done
+            if [ "$USE_PLATFORM_CERTIFICATE" = "true" ]; then
                 printf '\tcertificate: "platform",\n'
             fi
         elif [ "$CLASS" = "JAVA_LIBRARIES" ]; then
