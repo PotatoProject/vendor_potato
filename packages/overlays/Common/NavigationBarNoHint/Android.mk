45f3afaa8a3f0d6c568fd9bc598c036534655ffc
Jesse Chan <jc@lineageos.org>
2020-05-19 14:01:25 +0800

    vendor: Implement hide gestural navigation hint bar
    Change-Id: I5ed5c55ec46c3bb5e91aeb88bd5f06bd4445c2f0
    Signed-off-by: Jesse Chan <jc@lineageos.org>

---
+++ packages/overlays/Common/NavigationBarNoHint/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := NavigationBarNoHint
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := NavigationBarNoHint
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)
