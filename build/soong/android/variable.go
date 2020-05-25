a4a87d70e2494ab223ab2ee90b0bc26673512dda
rajadeja <rajsjadeja4@gmail.com>
2020-05-25 05:55:33 +0000

    vendor: Add Include_dirs in qcom_um param
    Change-Id: Ice11757fd5c318a0f59f7eb829d19ac09b9035aa

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -59,18 +59,22 @@ type Product_variables struct {
 		Shared_libs []string
 	}
 	Uses_qcom_um_3_18_family struct {
+		Include_dirs []string
 		Header_libs []string
 		Shared_libs []string
 	}
 	Uses_qcom_um_4_4_family struct {
+		Include_dirs []string
 		Header_libs []string
 		Shared_libs []string
 	}
 	Uses_qcom_um_4_9_family struct {
+		Include_dirs []string
 		Header_libs []string
 		Shared_libs []string
 	}
 	Uses_qcom_um_4_14_family struct {
+		Include_dirs []string
 		Header_libs []string
 		Shared_libs []string
 	}
