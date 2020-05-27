ef1267fb0b974b0333c93cc3e084aacd35167adc
rajadeja <rajsjadeja4@gmail.com>
2020-05-27 11:59:41 +0000

    Revert "vendor: Add qcom platform type to soongs vars"
    This reverts commit 5959123bfd783ded1e1608f0cf3318f8c9b9685f.
    
    Change-Id: Ibb8ad11b67ef5d0b6c13a0abd7e8aa47da8d9ad1

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -20,11 +20,6 @@ $(call add_json_bool, Target_needs_netd_direct_connect_rule, $(filter true,$(TAR
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
-$(call add_json_bool, Uses_qcom_um_family, $(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
-$(call add_json_bool, Uses_qcom_um_3_18_family, $(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
-$(call add_json_bool, Uses_qcom_um_4_4_family, $(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
-$(call add_json_bool, Uses_qcom_um_4_9_family, $(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
-$(call add_json_bool, Uses_qcom_um_4_14_family, $(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
