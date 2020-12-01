866d15866f410e21ac3c2735b4cd1b08f17f7d8b
ezio84 <brabus84@gmail.com>
2020-12-01 17:45:46 +0000

    Use a custom build date-time prop to refresh packages cache [2/2]
    Change-Id: I56ab415a2052eb37476e08890f44389b72d99579

--- config/branding.mk
+++ config/branding.mk
@@ -1,6 +1,7 @@
 GET_VERSION := vendor/potato/build/tools/getversion.py
 POTATO_VERNUM := $(shell $(GET_VERSION) vernum)
 POTATO_DISH := $(shell $(GET_VERSION) dish)
+BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
 BUILD_TYPE := $(shell $(GET_VERSION) buildtype)
 POTATO_BRANDING_VERSION := $(BUILD_TYPE)
 POTATO_VERSION := $(shell $(GET_VERSION) version)
@@ -12,3 +13,6 @@ endif
 ifneq ($(SIGNING_KEYS),)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := $(SIGNING_KEYS)/releasekey
 endif
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.datetime=$(BUILD_DATE_TIME)
