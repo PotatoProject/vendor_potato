1c0b2a2a4593b386b5ad04dbf44b333b6b1013d6
Raj Jadeja <rajsjadeja4@gmail.com>
2020-12-19 17:09:23 +0000

    potato: add QCOM namespace for UM
    Change-Id: Iaad32869bf0788e830808d3983ec48bf2704e3f7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -52,6 +52,12 @@ SOONG_CONFIG_customQcomVars += \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device
 
+# Only create soong_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
+ifneq ($(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),)
+SOONG_CONFIG_customQcomVars += \
+    qcom_soong_namespace
+endif
+
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -76,3 +82,6 @@ SOONG_CONFIG_customQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_customQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_customQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
+ifneq ($(filter $(UM_PLATFORMS),$(PRODUCT_BOARD_PLATFORM)),)
+SOONG_CONFIG_customQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif
