ba32918becf0010fed4a3d170a2886f023026964
TheMalachite <eliasgheeraert@gmail.com>
2021-01-27 00:03:34 +0100

    qcom: Add bengal platform
    - Also, properly add 4.19 platforms
    
    Change-Id: Ia9f602a1d6f109554c6462eaa02214c43b9295dd
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: TheMalachite <eliasgheeraert@gmail.com>

--- config/ProductConfigQcom.mk
+++ config/ProductConfigQcom.mk
@@ -4,6 +4,7 @@ include vendor/potato/build/core/qcom_utils.mk
 # Platform names
 KONA := kona #SM8250
 LITO := lito #SM7250
+BENGAL ?= bengal #SM6115
 MSMNILE := msmnile #SM8150
 MSMSTEPPE := sm6150
 TRINKET := trinket #SM6125
@@ -16,7 +17,7 @@ UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
-UM_4_19_FAMILY := $(KONA) $(LITO)
+UM_4_19_FAMILY := $(KONA) $(LITO) $(BENGAL)
 UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
 
