c66ea597d4a9a919dc6db919fc952708cb9b1aa7
JamieHoSzeYui <henloboii@protonmail.ch>
2021-03-17 14:44:44 +0800

    qcom: add lahaina platform
    lahaina = Snapdragon 888
    
    [@SahilSonar - POSP]
    - Adapt for our custom soong product namespace
    
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: Iaad90031451a9d1d3c8cbe8b61c1cf19717df4ee

--- config/ProductConfigQcom.mk
+++ config/ProductConfigQcom.mk
@@ -9,6 +9,7 @@ MSMNILE := msmnile #SM8150
 MSMSTEPPE := sm6150
 TRINKET := trinket #SM6125
 ATOLL := atoll #SM6250
+LAHAINA := lahaina #SM8350
 
 B_FAMILY := msm8226 msm8610 msm8974
 B64_FAMILY := msm8992 msm8994
@@ -18,10 +19,29 @@ UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_FAMILY := $(KONA) $(LITO) $(BENGAL)
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
-QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
+UM_5_4_FAMILY := $(LAHAINA)
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
+QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
 
 BOARD_USES_ADRENO := true
+
+# Add qtidisplay to soong config namespaces
+SOONG_CONFIG_NAMESPACES += qtidisplay
+
+# Add supported variables to qtidisplay config
+SOONG_CONFIG_qtidisplay += \
+    drmpp \
+    headless \
+    llvmsa \
+    gralloc4 \
+    default
+
+# Set default values for qtidisplay config
+SOONG_CONFIG_qtidisplay_drmpp ?= false
+SOONG_CONFIG_qtidisplay_headless ?= false
+SOONG_CONFIG_qtidisplay_llvmsa ?= false
+SOONG_CONFIG_qtidisplay_gralloc4 ?= false
+SOONG_CONFIG_qtidisplay_default ?= true
 
 # UM platforms no longer need this set on O+
 ifneq ($(call is-board-platform-in-list, $(UM_PLATFORMS)),true)
@@ -51,8 +71,13 @@ TARGET_USES_COLOR_METADATA := true
 endif
 
 # Enable DRM PP driver on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)),true)
 TARGET_USES_DRM_PP := true
+endif
+
+# Enable Gralloc4 on UM platforms that support it
+ifneq ($(call is-board-platform-in-list, $(UM_5_4_FAMILY)),true)
+    SOONG_CONFIG_qtidisplay_gralloc4 := true
 endif
 
 # Mark GRALLOC_USAGE_HW_2D, GRALLOC_USAGE_EXTERNAL_DISP and GRALLOC_USAGE_PRIVATE_WFD as valid gralloc bits
@@ -62,12 +87,12 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 13)
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 21)
 
 # Mark GRALLOC_USAGE_PRIVATE_HEIF_VIDEO as valid gralloc bits on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_5_4_FAMILY)),true)
     TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 27)
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 $(UM_4_14_FAMILY) $(UM_4_19_FAMILY)
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY)
 
 ALLOWED_SELINUX_VIOLATORS += \
     $(B_FAMILY) \
@@ -99,6 +124,9 @@ QCOM_HARDWARE_VARIANT := sm8150
 else ifeq ($(call is-board-platform-in-list, $(UM_4_19_FAMILY)),true)
 MSM_VIDC_TARGET_LIST := $(UM_4_19_FAMILY)
 QCOM_HARDWARE_VARIANT := sm8250
+else ifeq ($(call is-board-platform-in-list, $(UM_5_4_FAMILY)),true)
+MSM_VIDC_TARGET_LIST := $(UM_5_4_FAMILY)
+QCOM_HARDWARE_VARIANT := sm8350
 else
 MSM_VIDC_TARGET_LIST := $(PRODUCT_BOARD_PLATFORM)
 QCOM_HARDWARE_VARIANT := $(PRODUCT_BOARD_PLATFORM)
