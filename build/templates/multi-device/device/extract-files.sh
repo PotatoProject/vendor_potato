e449bfcb264e7a96d032d9921ba318fc8984cd61
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-09-10 16:06:48 +0200

    build: Sync templates with android_tools_extract-utils@55ec886
    Change-Id: I9df10ee96f6dc98dfb7c9f08fb77d2015c926256

---
+++ build/templates/multi-device/device/extract-files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2021 The Potato Open Sauce Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libsample1.so)
+            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
+            ;;
+        vendor/lib64/libsample2.so)
+            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
+            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
+            ;;
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=**** FILL IN DEVICE NAME ****
+export DEVICE_COMMON=**** FILL IN COMMON NAME ****
+export VENDOR=**** FILL IN VENDOR NAME ****
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
---
+++ build/templates/multi-device/device/setup-makefiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2021 The Potato Open Sauce Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+export DEVICE=**** FILL IN DEVICE NAME ****
+export DEVICE_COMMON=**** FILL IN COMMON NAME ****
+export VENDOR=**** FILL IN VENDOR NAME ****
+
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
