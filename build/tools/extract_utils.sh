hentaiOS/platform_vendor_hentai

extract_utils: Use new common prebuilts dir
Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
Change-Id: I174b9ebd3c8b1ad164e39f30e719dee6d08578e8

build/tools/extract_utils.sh
@@ -96,6 +96,7 @@ function setup_vendor() {
     fi
 
     export BINARIES_LOCATION="$HENTAI_ROOT"/prebuilts/extract-tools/${HOST}-x86/bin
+    export JARS_LOCATION="$HENTAI_ROOT"/prebuilts/extract-tools/common/smali
 
     for version in 0_8 0_9; do
         export PATCHELF_${version}="$BINARIES_LOCATION"/patchelf-"${version}"
@@ -1206,16 +1207,16 @@ function oat2dex() {
     local OAT=
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
-        export BAKSMALIJAR="$HENTAI_ROOT"/prebuilts/tools-hentai/common/smali/baksmali.jar
-        export SMALIJAR="$HENTAI_ROOT"/prebuilts/tools-hentai/common/smali/smali.jar
+        export BAKSMALIJAR="$JARS_LOCATION"/baksmali.jar
+        export SMALIJAR="$JARS_LOCATION"/smali.jar
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$HENTAI_ROOT"/prebuilts/tools-hentai/${HOST}-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$BINARIES_LOCATION"/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$HENTAI_ROOT"/prebuilts/tools-hentai/${HOST}-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$BINARIES_LOCATION"/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder

PotatoProject/vendor_potato

build/tools/extract_utils.sh
@@ -95,6 +95,7 @@ function setup_vendor() {
     fi
 
     export BINARIES_LOCATION="$POTATO_ROOT"/prebuilts/extract-tools/${HOST}-x86/bin
+    export JARS_LOCATION="$POTATO_ROOT"/prebuilts/extract-tools/common/smali
 
     for version in 0_8 0_9; do
         export PATCHELF_${version}="$BINARIES_LOCATION"/patchelf-"${version}"
@@ -1209,16 +1210,16 @@ function oat2dex() {
     local OAT=
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
-        export BAKSMALIJAR="$POTATO_ROOT"/prebuilts/tools-lineage/common/smali/baksmali.jar
-        export SMALIJAR="$POTATO_ROOT"/prebuilts/tools-lineage/common/smali/smali.jar
+        export BAKSMALIJAR="$JARS_LOCATION"/baksmali.jar
+        export SMALIJAR="$JARS_LOCATION"/smali.jar
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$POTATO_ROOT"/prebuilts/tools-lineage/${HOST}-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$BINARIES_LOCATION"/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$POTATO_ROOT"/prebuilts/tools-lineage/${HOST}-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$BINARIES_LOCATION"/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder