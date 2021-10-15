ba828c2af0645c38a4666c7c27ac24f4fbfb152e
SahilSonar <sss.sonar2003@gmail.com>
2021-10-15 12:55:01 +0200

    potato: overlay: Drop obsolete overlays
    Change-Id: I0b83154ba479a8a49fd4b8a7fc12573bd7e19c52

--- config/packages.mk
+++ config/packages.mk
@@ -2,15 +2,6 @@
 PRODUCT_PACKAGES += \
     librsjni
 
-# Themes
-PRODUCT_PACKAGES += \
-    SystemDarkGrayOverlay \
-    SystemUIDarkGrayOverlay \
-    SystemStyleOverlay \
-    SystemUIStyleOverlay \
-    SystemNightOverlay \
-    SystemUINightOverlay
-
 # Fries
 PRODUCT_PACKAGES += \
     PotatoFries
@@ -33,10 +24,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NavigationBarNoHint \
     NavigationBarMode2ButtonOverlay
-
-# QS Color Overlay
-PRODUCT_PACKAGES += \
-    QsColor
 
 # SystemUI plugins
 PRODUCT_PACKAGES += \
