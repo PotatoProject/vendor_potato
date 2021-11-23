c6a1bbaebbf6fe573e91ee28a0af16bbff5146cb
SahilSonar <sss.sonar2003@gmail.com>
2021-11-23 09:55:59 +0100

    config: Update default ringtone, notification and alarm from SD1A.210817.037
    Change-Id: I74a580ec0c6937ba3f83523a5048670180480241

--- config/sounds.mk
+++ config/sounds.mk
@@ -19,6 +19,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ui,$(TARGET_
 
 # Set default ringtone, notification and alarm
 PRODUCT_PRODUCT_PROPERTIES += \
-   ro.config.alarm_alert=Bright_morning.ogg \
-   ro.config.ringtone=The_big_adventure.ogg \
-   ro.config.notification_sound=Popcorn.ogg
+    ro.config.ringtone=Your_new_adventure.ogg \
+    ro.config.notification_sound=Eureka.ogg \
+    ro.config.alarm_alert=Fresh_start.ogg
