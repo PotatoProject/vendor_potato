a184983255d3a7867f3f5f62c6df496fed6da46a
SahilSonar <sss.sonar2003@gmail.com>
2022-09-05 20:09:28 +0200

    potato: Begining of new things!
    TODO: Convert to RRO for overlays
    Change-Id: I828423c2bd3a3dd5431288197039ab58b1cf9bbf

--- config/common.mk
+++ config/common.mk
@@ -6,15 +6,6 @@ endif
 
 PRODUCT_SOONG_NAMESPACES += $(PATHMAP_SOONG_NAMESPACES)
 
-# Required for QTI BT Stack
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
-else
-PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
-endif
-
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google
@@ -22,6 +13,10 @@ else
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/potato/overlay/common
+PRODUCT_ENFORCE_RRO_TARGETS += framework-res
 
 # Bootanimation
 PRODUCT_COPY_FILES += \
@@ -48,7 +43,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# POSP Common
+# POSP common
 PRODUCT_COPY_FILES += \
     vendor/potato/prebuilt/common/etc/permissions/co.potatoproject.posp.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/co.potatoproject.posp.xml \
 
@@ -56,28 +51,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/potato/prebuilt/common/etc/permissions/privapp-permissions-fries.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-fries.xml \
     vendor/potato/prebuilt/common/etc/sysconfig/potatofries-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/potatofries-hiddenapi-package-whitelist.xml
-
-# Fix Google dialer
-PRODUCT_COPY_FILES += \
-    vendor/potato/prebuilt/common/etc/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml
-
-# Set custom volume steps
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.media_vol_steps=30 \
-    ro.config.bt_sco_vol_steps=30
-
-# Power whitelist
-PRODUCT_COPY_FILES += \
-    vendor/potato/config/permissions/custom-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/custom-power-whitelist.xml
-
-# Clang
-ifeq ($(TARGET_USE_LATEST_CLANG),true)
-    TARGET_KERNEL_CLANG_VERSION := $(shell grep -v based prebuilts/clang/host/$(HOST_OS)-x86/*/AndroidVersion.txt | sort | tail -n 1 | cut -d : -f 2)
-endif
-
-# Disable Rescue Party
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.disable_rescue=true
 
 # Enable one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -92,10 +65,6 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat
 endif
-
-# Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/potato/overlay/common
-PRODUCT_ENFORCE_RRO_TARGETS += framework-res
 
 # GMS
 ifeq ($(WITH_GMS), true)
