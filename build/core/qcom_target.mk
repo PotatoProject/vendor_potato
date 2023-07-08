103ab6c975e2cd06424b5b54531ca7e4f5178dc6
SahilSonar <sss.sonar2003@gmail.com>
2023-07-08 12:46:35 +0200

    qcom_target: Remove vendor/qcom/opensource/data-ipa-cfg-mgr pathmap
      - Needed after, a87cd2b7b544425f409489435724825ab6c78163 to avoid adding both legacy
        and non legacy data-ipa-cfg-mgr to soong namespace.

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -18,7 +18,6 @@ $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWAR
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
-$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
 $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
